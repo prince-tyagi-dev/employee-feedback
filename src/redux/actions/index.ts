0138110fd05aaa6bccfffb6964543cc60674ecbd
Prince Tyagi <prince.tyagi@xebia.com>
2022-06-22 20:16:09 +0530

    Remove redux for now as currently we are not using this.

--- src/redux/actions/index.ts
+++
@@ -1,9 +0,0 @@
-import IEmployee from "../../Interfaces/Employee";
-import { IAction } from "../../Interfaces/Common";
-import { LOGIN } from "../types";
-
-const saveLoginInfo = (payload: IEmployee): IAction => {
-  return { type: LOGIN, payload };
-};
-
-export { saveLoginInfo };
--- src/redux/reducers/rootReducer/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { IReducer } from "../../../Interfaces/Common";
-import loginReducer from "../loginReducer";
-
-const reducersInfo: IReducer = { login: loginReducer };
-const rootReducer = combineReducers(reducersInfo);
-
-export default rootReducer;
